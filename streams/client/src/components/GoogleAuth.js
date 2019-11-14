599e7fe0847142b8ad3845130923adbecface58e
Alex <alex94.design@gmail.com>
2019-11-14 16:43:09 +0000

    setting up google authentication

---
+++ streams/client/src/components/GoogleAuth.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client.init({
+        clientId:
+          "593945183791-60j4qupo80sgr6kf9kbka36o3bjeoaul.apps.googleusercontent.com",
+        scope: "email"
+      });
+    });
+  }
+
+  render() {
+    return <div>Google Auth</div>;
+  }
+}
+
+export default GoogleAuth;
