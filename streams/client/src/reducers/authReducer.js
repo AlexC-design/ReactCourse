f4ccc98b20ec53c8b0057c0e983bfae6ed5bae9d
AlexC-Design <alex94.design@gmail.com>
2019-11-15 15:12:05 +0000

    changed action types from strings to variables to prevent errors from typos

--- streams/client/src/reducers/authReducer.js
+++ streams/client/src/reducers/authReducer.js
@@ -1,8 +1,10 @@
+import { SIGN_IN, SIGN_OUT } from "../actions/types";
+
 export default (state = { isSignedIn: null }, action) => {
   switch (action.type) {
-    case "SIGN_IN":
+    case SIGN_IN:
       return { ...state, isSignedIn: true };
-    case "SIGN_OUT":
+    case SIGN_OUT:
       return { ...state, isSignedIn: false };
     default:
       return state;
