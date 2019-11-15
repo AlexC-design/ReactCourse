b5e9efb37df1ac2fe1704e7d74c2cd19474bc882
AlexC-Design <alex94.design@gmail.com>
2019-11-15 15:04:04 +0000

    changed handling the sign in/out functionality from GoogleAuth component to redux

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -1,8 +1,8 @@
 import React from "react";
+import { connect } from "react-redux";
+import { signIn, signOut } from "../actions";
 
 class GoogleAuth extends React.Component {
-  state = { isSignedIn: null };
-
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
       window.gapi.client
@@ -13,19 +13,24 @@ class GoogleAuth extends React.Component {
         })
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
-          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.onAuthChange(this.auth.isSignedIn.get());
           this.auth.isSignedIn.listen(this.onAuthChange);
         });
     });
   }
 
-  onAuthChange = () => {
-    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  //this gets called with a bool argument
+  onAuthChange = isSignedIn => {
+    if (isSignedIn) {
+      this.props.signIn();
+    } else {
+      this.props.signOut();
+    }
   };
 
   renderAuthButton() {
-    if (this.state.isSignedIn === null) return null;
-    else if (this.state.isSignedIn)
+    if (this.props.isSignedIn === null) return null;
+    else if (this.props.isSignedIn)
       return (
         <button onClick={this.auth.signOut} className="ui red basic button">
           Sign out
@@ -45,4 +50,8 @@ class GoogleAuth extends React.Component {
   }
 }
 
-export default GoogleAuth;
+const mapStateToProps = state => {
+  return { isSignedIn: state.auth.isSignedIn };
+};
+
+export default connect(mapStateToProps, { signIn, signOut })(GoogleAuth);
--- streams/client/src/reducers/index.js
+++ streams/client/src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
+import authReducer from "./authReducer";
 
-export default cmobineRducers({
-  replaceMe: "123"
+export default combineReducers({
+  auth: authReducer
 });
