5cd5633fd1b6a933f4a0ae0228769bbf4bfb0624
AlexC-Design <alex94.design@gmail.com>
2019-11-15 18:32:03 +0000

    refactored renderInput from StreamCreate

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -2,14 +2,20 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
-  renderInput(formProps) {
-    return (
-      <input
-        onChange={formProps.input.onChange}
-        value={formProps.input.value}
-      />
-    );
+  renderInput({ input }) {
+    //de-structured 'input' from formProps
+    return <input {...input} />; //adding all key-value pairs from 'input'(of formProps) as props to the input element
   }
+
+  // --above function before refactor
+  // renderInput(formProps) {
+  //   return (
+  //     <input
+  //       onChange={formProps.input.onChange}
+  //       value={formProps.input.value}
+  //     />
+  //   );
+  // }
 
   render() {
     return (
