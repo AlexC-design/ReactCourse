a9942f83a2c5ecc042c8dd30991a09c5b24e244d
AlexC-Design <alex94.design@gmail.com>
2019-11-17 12:51:04 +0000

    linking streamId from the URL to the StreamEdit component

--- streams/client/src/components/streams/StreamEdit.js
+++ streams/client/src/components/streams/StreamEdit.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const StreamEdit = () => {
+const StreamEdit = props => {
+  console.log(props);
   return <div>StreamEdit</div>;
 };
 
-export default StreamEdit;
+mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect()(StreamEdit);
