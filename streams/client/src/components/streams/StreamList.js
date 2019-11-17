3d0a02613cb04b08849e8def5521bca11baf0873
AlexC-Design <alex94.design@gmail.com>
2019-11-17 11:59:40 +0000

    creating and displaying stream edit/delete/create buttons

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -1,16 +1,29 @@
 import React from "react";
 import { connect } from "react-redux";
 import { fetchStreams } from "../../actions/";
+import { Link } from "react-router-dom";
 
 class SteamList extends React.Component {
   componentDidMount() {
     this.props.fetchStreams();
   }
 
+  renderAdminButtons(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button blue">Edit</button>
+          <button className="ui button basic red">Delete</button>
+        </div>
+      );
+    }
+  }
+
   renderList() {
     return this.props.streams.map(stream => {
       return (
         <div className="item" key={stream.id}>
+          {this.renderAdminButtons(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
             {stream.title}
@@ -21,18 +34,35 @@ class SteamList extends React.Component {
     });
   }
 
+  renderCreateButton() {
+    if (this.props.isSignedIn) {
+      return (
+        <div style={{ textAlign: "right" }}>
+          <Link to="/streams/new" className="ui button light green">
+            Create Stream!
+          </Link>
+        </div>
+      );
+    }
+  }
+
   render() {
     return (
       <div>
         <h2>Streams</h2>
         <div className="ui celled list">{this.renderList()}</div>
+        {this.renderCreateButton()}
       </div>
     );
   }
 }
 
 const mapStateToProps = state => {
-  return { streams: Object.values(state.streams) };
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn
+  };
 };
 
 export default connect(mapStateToProps, { fetchStreams })(SteamList);
