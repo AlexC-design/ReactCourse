89e85c13038aece1589bfff06e08ddf381c77c9e
AlexC-Design <alex94.design@gmail.com>
2019-11-19 13:52:24 +0000

    Changed the app to use hooks with functional components instead of class based components

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,31 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 import Loader from "./Loader";
+import useLocation from "./useLocation";
 
-class App extends React.Component {
-  state = { lat: null, errorMessage: "" };
+const App = () => {
+  const [lat, errorMessage] = useLocation();
 
-  componentDidMount() {
-    window.navigator.geolocation.getCurrentPosition(
-      position => this.setState({ lat: position.coords.latitude }),
-      err => this.setState({ errorMessage: err.message })
-    );
-  }
+  let content;
+  if (errorMessage) {
+    content = <div>Error: {errorMessage}</div>;
+  } else if (lat) {
+    content = <SeasonDisplay lat={lat} />;
+  } else content = <Loader />;
 
-  renderContent() {
-    if (this.state.errorMessage && !this.state.lat)
-      return <div>Error: {this.state.errorMessage}</div>;
-
-    if (!this.state.errorMessage && this.state.lat)
-      return <SeasonDisplay lat={this.state.lat} />;
-
-    return <Loader />;
-  }
-
-  render() {
-    return <div>{this.renderContent()}</div>;
-  }
-}
+  return <div>{content}</div>;
+};
 
 ReactDOM.render(<App />, document.querySelector("#root"));
