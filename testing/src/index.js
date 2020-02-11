ba21444d176544c38f84c6d8322bd332fdb5f3f7
Alex C <56199756+AlexC-design@users.noreply.github.com>
2020-02-11 11:52:34 +0000

    separating redux store creation in a different component and fixing tests for components requiring access to the store

--- testing/src/index.js
+++ testing/src/index.js
@@ -1,16 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, compose } from "redux";
-import reducers from "reducers/";
+import Root from "Root";
 
 import App from "./components/App";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
 ReactDOM.render(
-  <Provider store={createStore(reducers, composeEnhancers())}>
+  <Root>
     <App />
-  </Provider>,
+  </Root>,
   document.querySelector("#root")
 );
