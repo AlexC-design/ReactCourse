5e6adb12afcf4e860928983e64fdb8efdd705de7
Alex C <56199756+AlexC-design@users.noreply.github.com>
2020-02-11 11:36:22 +0000

    wiring up redux dev tools

--- testing/src/index.js
+++ testing/src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, compose } from "redux";
 import reducers from "reducers/";
 
 import App from "./components/App";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 ReactDOM.render(
-  <Provider store={createStore(reducers, {})}>
+  <Provider store={createStore(reducers, composeEnhancers())}>
     <App />
   </Provider>,
   document.querySelector("#root")
